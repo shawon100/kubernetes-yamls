0082f8a6491977435c02356090f6f077ecb2bb36
shawon100 <shawonashadullah@gmail.com>
2022-08-29 23:02:03 +0600

    added helm install script and yaml

---
+++ Helm/helminstall-nginx.sh
@@ -0,0 +1,17 @@
+#src : https://docs.microsoft.com/en-us/azure/aks/ingress-tls
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# changes context to stage
+kubectl config use-context your-cluster-name
+
+# Create a namespace for your ingress resources
+kubectl create namespace nginx-ingress
+
+# Use Helm to deploy an NGINX ingress controller
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+    --namespace nginx-ingress \
+    --set controller.replicaCount=3 \
+    -f internal-ingress-web.yaml \
+    --set controller.nodeSelector."kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux
+
